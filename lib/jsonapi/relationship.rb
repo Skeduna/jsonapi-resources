2b950fded293640e916ff638d3483fec0c145a0f
ggalley <ggalley@skeduna.com>
2016-01-20 15:42:57 -0700

    Some namespace type naming

--- lib/jsonapi/relationship.rb
+++ lib/jsonapi/relationship.rb
@@ -25,6 +25,36 @@ module JSONAPI
       @resource_klass = @parent_resource.resource_for(@class_name)
     end
 
+    def relation_for_type(name, class_name, options)
+
+      #if options has classname and does not equal the object.name then
+      #then parse from class name.
+      if class_name == name
+        class_name
+      else
+          #   zjsonb_publishers
+          # {:class_name=>"Api::V1::Zjsonbs::Publisher", :parent_resource=>Api::V1::Zjsonas::BookResource}
+
+        if options.has_key?(:class_name) && options.has_key?(:parent_resource)
+
+          class_array = class_name.downcase.split('::')
+          parent_array = options[:parent_resource].to_s.downcase.split('::')
+          parent_array.each do |item|
+            if class_array.include?(item)
+              class_array.delete(item)
+            end
+          end
+          if class_array.empty?
+            'UNKONWN RELATION TYPE 48'
+          else
+            class_array.join('_')
+          end
+        else
+          'UNKONWN RELATION TYPE 53'
+        end
+      end
+    end
+
     def relation_name(options)
       case @relation_name
         when Symbol
@@ -54,11 +84,11 @@ module JSONAPI
         super
 
         @class_name = options.fetch(:class_name, name.to_s.camelize)
-        puts "ToOne: #{class_name.underscore.pluralize.to_sym} name #{name}"
+        #puts "ToOne: #{class_name.underscore.pluralize.to_sym} name #{name}"
         #puts "ToOne: #{class_name.underscore.pluralize.to_sym}"
-        binding.pry if "#{class_name.underscore.pluralize.to_sym}" == 'api/v1/students/exams'
+        binding.pry if "#{class_name.underscore.pluralize.to_sym}" == 'api/v1/zjsonbs/publishers'
         #myname = "#{class_name.to_s.underscore.singularize}_resource".camelize
-        @type = class_name.underscore.pluralize.to_sym
+        @type = relation_for_type(name.to_s.camelize, @class_name, options).underscore.pluralize.to_sym
         @foreign_key ||= "#{name}_guid".to_sym
         @foreign_key_on = options.fetch(:foreign_key_on, :self)
       end
@@ -77,8 +107,9 @@ module JSONAPI
 
         super
         @class_name = options.fetch(:class_name, name.to_s.camelize.singularize)
-        puts "ToMany: #{class_name.underscore.pluralize.to_sym} name #{name}"
-        @type = class_name.underscore.pluralize.to_sym
+        #puts "ToMany: #{class_name.underscore.pluralize.to_sym} name #{name}"
+        @type = relation_for_type(name.to_s.camelize.singularize, @class_name, options).underscore.pluralize.to_sym
+        #@type = class_name.underscore.pluralize.to_sym
         @foreign_key ||= "#{name.to_s.singularize}_guids".to_sym
       end
     end
